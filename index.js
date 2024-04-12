ef6ae171b06bd6060dc31208ea67017c5a3cd376
Rethabile Diale <rethadiale27@gmail.com>
2024-04-12 05:36:41 +0200

    Aligning the blocks of code

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-try {
 // Fetch a random landscape-oriented nature photo from Unsplash API
-const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-const data = await res.json()
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
 
 // Set the background image of the document body to the fetched image
- document.body.style.backgroundImage = `url(${data.urls.regular})`;
-// Set the author name in the HTML element with id "author"
- document.getElementById("author").textContent = `By: ${data.user.name}`; 
-} catch (err) {
-     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
 
-     document.getElementById("author").textContent = `By: Rethabile Diale`;
-     }
+// Set the author name in the HTML element with id "author"
+    document.getElementById("author").textContent = `By: ${data.user.name}`; 
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+
+    document.getElementById("author").textContent = `By: Rethabile Diale`;
+    }
     
 // Fetch Dogecoin data from CoinGecko API     
 try {
@@ -21,15 +22,15 @@ try {
     }
     const data = await res.json()
 // Display Dogecoin data in the HTML
- document.getElementById("crypto-top").innerHTML = `
-    <img src=${data.image.small} />
-    <span>${data.name}</span>
-`     
- document.getElementById ("crypto").innerHTML += `
-     <p>🎯: $${data.market_data.current_price.usd}</p>
-     <p>👆: $${data.market_data.high_24h.usd}</p>
-     <p>👇: $${data.market_data.low_24h.usd}</p>  
-`         
+    document.getElementById("crypto-top").innerHTML = `
+      <img src=${data.image.small} />
+      <span>${data.name}</span>
+    `     
+    document.getElementById ("crypto").innerHTML += `
+      <p>🎯: $${data.market_data.current_price.usd}</p>
+      <p>👆: $${data.market_data.high_24h.usd}</p>
+      <p>👇: $${data.market_data.low_24h.usd}</p>  
+    `         
  } catch (err) {
      console.error(err);
  }
